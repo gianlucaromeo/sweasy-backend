f8fe0e5098d468c26abb1ec5d2cd936748816eec
gianluca-romeo <gianlucaromeo@outlook.com>
2025-08-28 23:39:39 +0200

    fix(catalog): Delete wrong endpoint pointing to catalog urls

--- sweasy/urls.py
+++ sweasy/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('catalog.urls')),
+    path('catalogs/', include('catalog.urls')),
     path('admin/', admin.site.urls),
 ]
