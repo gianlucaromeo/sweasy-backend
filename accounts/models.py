fb6796f243a7018fc35824b77d0138125efe7b75
gianluca-romeo <gianlucaromeo@outlook.com>
2025-09-05 20:46:32 +0200

    chore: Add todo

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,23 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-
+    
+    # TODO: Add:
+    #
+    # - password_changed_at
+    # - last_login_at
+    # - reset_requested_at
+    # - reset_completed_at
+    
+    # Optional:
+    #
+    # - is_ban
+    # - activated_at
+    # - is_mfa_enabled
+    # - mfa_secret_changed_at
+    # - mfa_reset_at
+    # - ...
+    
+    
     def __str__(self):
         return self.username
