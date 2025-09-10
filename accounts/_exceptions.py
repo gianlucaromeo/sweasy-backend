71dc6fe4ed93925a27b0abc07a4e133cfd5e0c62
gianluca-romeo <gianlucaromeo@outlook.com>
2025-09-10 23:23:41 +0200

    refactor: Move registration errors handling to dedicated handler

---
+++ accounts/_exceptions.py
@@ -0,0 +1,61 @@
+from rest_framework.views import exception_handler as drf_handler
+from rest_framework.response import Response
+from rest_framework import status
+
+from accounts.constants import (
+    CODE_USERNAME_REQUIRED,
+    CODE_EMAIL_REQUIRED,
+    CODE_PASSWORD1_REQUIRED,
+    CODE_PASSWORD2_REQUIRED,
+    CODE_USERNAME_UNIQUE,
+    CODE_EMAIL_UNIQUE,
+    CODE_PASSWORDS_DO_NOT_MATCH,
+)
+
+
+def registration_exception_handler(exc, ctx):
+    resp = drf_handler(exc, ctx)
+    if resp is None or not isinstance(resp.data, dict):
+        print("Resp is None or not isinstance(resp.data, dict):", resp)
+        return resp
+
+    data = resp.data
+
+    def is_required(e):
+        return getattr(e, "code", "") == "required"
+
+    def is_unique(e, unique_code="unique"):
+        return getattr(e, "code", "") == unique_code
+
+    required_fields = {
+        "username": CODE_USERNAME_REQUIRED,
+        "email": CODE_EMAIL_REQUIRED,
+        "password1": CODE_PASSWORD1_REQUIRED,
+        "password2": CODE_PASSWORD2_REQUIRED,
+    }
+
+    for field, code in required_fields.items():
+        if any(is_required(e) for e in data.get(field, [])):
+            return Response({"code": code}, status=status.HTTP_400_BAD_REQUEST)
+
+    if any(is_unique(e) for e in data.get("username", [])):
+        return Response(
+            {"code": CODE_USERNAME_UNIQUE},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if any(is_unique(e, "email-unique") for e in data.get("email", [])):
+        return Response(
+            {"code": CODE_EMAIL_UNIQUE},
+            status=status.HTTP_201_CREATED,
+        )
+
+    if "non_field_errors" in data:
+        for error in data["non_field_errors"]:
+            if CODE_PASSWORDS_DO_NOT_MATCH in error.code:
+                return Response(
+                    {"code": CODE_PASSWORDS_DO_NOT_MATCH},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+    return resp
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,13 @@
 import jwt
 
-from django.db.utils import IntegrityError
 from django.utils import timezone
 from django.conf import settings
-from rest_framework import generics, serializers
+from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from rest_framework.views import Response, status
+from rest_framework.views import Response
+from rest_framework import status
 from rest_framework_simplejwt.views import TokenObtainPairView
+from accounts._exceptions import registration_exception_handler
 from accounts.constants import *
 from .models import User
 from .serializers import RegisterSerializer, LoginSerializer
@@ -20,71 +21,9 @@ from .utils import (
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     queryset = User.objects.all()
-
-    def __is_missing_required(self, field: str, detail: dict) -> bool:
-        if field in detail:
-            if "required" in detail[field][0].code:
-                return True
-        return False
-
-    def __is_not_unique(self, field: str, detail: dict) -> bool:
-        if field in detail:
-            if "unique" in detail[field][0].code:
-                return True
-        return False
-
-    def __code_400(self, code):
-        return Response(
-            {"code": code},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-
-    def __created_details_201(self, detail: str) -> Response:
-        return Response(
-            {"detail": detail},
-            status=status.HTTP_201_CREATED,
-        )
-
-    def __unhandled_exception_details_400(self, detail: str) -> Response:
-        return Response(
-            data={"detail": detail},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-
-    def __handle_validation_error(
-        self,
-        err: serializers.ValidationError,
-    ) -> Response:
-        detail = err.detail if isinstance(err.detail, dict) else {}
-
-        required_fields = [
-            {"field": "username", "code": CODE_USERNAME_REQUIRED},
-            {"field": "email", "code": CODE_EMAIL_REQUIRED},
-            {"field": "password1", "code": CODE_PASSWORD1_REQUIRED},
-            {"field": "password2", "code": CODE_PASSWORD2_REQUIRED},
-        ]
-
-        for field in required_fields:
-            if self.__is_missing_required(field["field"], detail):
-                return self.__code_400(field["code"])
-
-        # USERNAME is not unique
-        if self.__is_not_unique("username", detail):
-            return self.__code_400(CODE_USERNAME_UNIQUE)
-
-        # EMAIL is not unique
-        if self.__is_not_unique("email", detail):
-            return self.__created_details_201(GENERIC_REGISTRATION_MSG)
-
-        if "non_field_errors" in detail:
-            for error in detail["non_field_errors"]:
-                # Passwords do not match
-                if CODE_PASSWORDS_DO_NOT_MATCH in error.code:
-                    return self.__code_400(CODE_PASSWORDS_DO_NOT_MATCH)
-
-        # Other
-        print("Unhandled validation error:", str(err))
-        return self.__unhandled_exception_details_400(GENERIC_REGISTRATION_ERR_MSG)
+    
+    def get_exception_handler(self):
+        return registration_exception_handler
 
     def perform_create(self, serializer):
         serializer.save()
@@ -100,19 +39,6 @@ class RegisterView(generics.CreateAPIView):
             [serializer.validated_data["email"]],
             fail_silently=False,
         )
-
-    def create(self, request, *args, **kwargs):
-        try:
-            return super().create(request, *args, **kwargs)
-        except IntegrityError as e:
-            # Should be when unique constraint is violated
-            print("Integrity error:", str(e))
-            return self.__handle_integrity_error(e)
-        except serializers.ValidationError as e:
-            return self.__handle_validation_error(e)
-        except Exception as e:
-            print("Unhandled exception:", str(e))
-            return self.__unhandled_exception_details_400(GENERIC_REGISTRATION_ERR_MSG)
 
 
 class VerifyEmailView(generics.GenericAPIView):
