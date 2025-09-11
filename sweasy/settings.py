27fb6c20407368bee7ec85e5693bfe827c224668
gianluca-romeo <gianlucaromeo@outlook.com>
2025-09-11 22:41:32 +0200

    chore: Add default values for non existing os env variables and remove comments

--- sweasy/settings.py
+++ sweasy/settings.py
@@ -5,20 +5,11 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
 
-# SECURITY WARNING: keep the secret key used in production secret.
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production.
-DEBUG = os.getenv("DEBUG")
+DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-# A list of strings representing the host/domain names that this Django site
-# can serve.
-# When DEBUG is True and ALLOWED_HOSTS is empty, the host is validated against
-# ['.localhost', '127.0.0.1', '[::1]'].
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
-
-
-# Application definition
 
 INSTALLED_APPS = [
     "django_extensions",
@@ -45,8 +36,8 @@ REST_FRAMEWORK = {
 }
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-EMAIL_VERIFY_TTL_SECONDS = os.getenv("EMAIL_VERIFY_TTL_SECONDS")
-EMAIL_VERIFY_ALG = os.getenv("EMAIL_VERIFY_ALG")
+EMAIL_VERIFY_TTL_SECONDS = os.getenv("EMAIL_VERIFY_TTL_SECONDS", 900)
+EMAIL_VERIFY_ALG = os.getenv("EMAIL_VERIFY_ALG", "HS256")
 
 ROOT_URLCONF = "sweasy.urls"
 
@@ -56,17 +47,9 @@ TEMPLATES = [
         "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
-            # A list of callables that take a request object as their argument
-            # and return a dictionary of items to be merged into the context.
-            # Note: Each context processor is applied in order.
             "context_processors": [
-                # This allows RequestContext to contain "request".
                 "django.template.context_processors.request",
-                # This allows RequestContext to contain "user", "perms".
                 "django.contrib.auth.context_processors.auth",
-                # This allows RequestContext to contain "messages" and
-                # "DEFAULT_MESSAGE_LEVELS".
-                # 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
@@ -74,20 +57,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "sweasy.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -104,26 +79,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.User"
